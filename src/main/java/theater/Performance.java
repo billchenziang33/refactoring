3d89925b64f0384e6131b4a4c4f1a5f2691dec76
billchenziang33 <billziang.chen@mail.utoronto.ca>
2025-11-16 10:45:40 +0800

    Task1.1

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -1,15 +1,33 @@
 package theater;
 
 /**
- * Class representing a performance of a play..
+ * Class representing a performance of a play.
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private final String playID;
+    private final int audience;
 
     public Performance(String playID, int audience) {
         this.playID = playID;
         this.audience = audience;
+    }
+
+    /**
+     * Returns the play ID for this performance.
+     *
+     * @return the play ID
+     */
+    public String getPlayID() {
+        return playID;
+    }
+
+    /**
+     * Returns the audience size for this performance.
+     *
+     * @return the audience count
+     */
+    public int getAudience() {
+        return audience;
     }
 }
